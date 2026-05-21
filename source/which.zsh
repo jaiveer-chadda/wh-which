eb646fc9d631680f5cecc6e5fbf49f2ab85b5b9b
JV <jaiveer.chadda@gmail.com>
2026-05-22 00:11:32 +0100

    Moved most of the constants (mostly colours) that were defined in the various subfunctions into the main function, so they can be reused
    Also redid a bunch of the variable naming to make it more sensible

--- source/which.zsh
+++ source/which.zsh
@@ -3,61 +3,74 @@
 wh() {
   setopt local_options warn_create_global warn_nested_var extended_glob
 
-  local -i 2 internal_mode=0 display_indices=0
-  local -i 2 draw_lines=1 do_all=1
+  local -r error="$0:"
 
+  local -i 2 internal_mode=0 do_indices=0
+  local -i 2 do_lines=1 do_all=1
+
+  local opt OPTIND OPTARG
   while { getopts 'xilA' opt; } { #
     case "$opt" {
-      ( x )   internal_mode=1 ;;
-      ( i ) display_indices=1 ;;
-      ( l )      draw_lines=0 ;;
-      ( A )          do_all=0 ;;
+      ( x ) internal_mode=1 ;;
+      ( i )    do_indices=1 ;;
+      ( l )      do_lines=0 ;;
+      ( A )        do_all=0 ;;
     }
   }
 
   shift $(( OPTIND - 1 ))
 
-  if (( internal_mode )) display_indices=0 draw_lines=0 do_all=0
+  if (( internal_mode )) do_indices=0 do_lines=0 do_all=0
 
   if ! (( $# )) {
-    echo "$0: must enter a command" >&2
+    echo "$error must enter a command" >&2
     return 1
   }
 
   # ————————————————————————————————————————————————————————————————— #
 
   local -r NL=$'\n'
-  local -r _line_char='─'
+  local -r sq=\' dq=\"
 
   local -r reset=$'\e[m'
+  local -r no_udln=$'\e[24m'
 
-  local -r _outside_col=$'\e[38;5;103m'  #8087A2
-  local -r  _inside_col=$'\e[38;5;238m'  #404454
-  local -ri 10 _line_len=$(( COLUMNS * 0.8 ))
+  local -r     d_red=$'\e[38;5;131m'  #B84C50
+  local -r     green=$'\e[38;5;114m'  #A6E3A1
+  local -r turquoise=$'\e[38;5;116m'  #94E2D5
+  local -r    d_blue=$'\e[38;5;60m'   #353F5A
+  local -r    m_blue=$'\e[38;5;68m'   #6D8EC5
+  local -r    l_blue=$'\e[38;5;111m'  #89B4FA
+  local -r      pink=$'\e[38;5;183m'  #CBA6F7
+  local -r    d_grey=$'\e[38;5;238m'  #404454
+  local -r    l_grey=$'\e[38;5;103m'  #8087A2
+  local -r blue_udln=$'\e[58;5;68;4m' #6D8EC5
 
-  local -r outside_line="$_outside_col${(pr:$_line_len::$_line_char:)}$reset"
-  local -r  inside_line="$_inside_col${(pr:$_line_len::$_line_char:)}$reset"
+  local -ri 10 line_len=$(( COLUMNS * 0.6 ))
+
+  local -r outside_line="$l_grey${(r:$line_len::─:)}$reset"
+  local -r  inside_line="$d_grey${(r:$line_len::─:)}$reset"
 
   # ————————————————————————————————————————————————————————————————— #
 
   local -r command="$1"
 
-  local -r _alias_prefix="$command: aliased to "
-  local -r _builtin_line="$command: shell built-in command"
-  local -r _file_prefix='/'
+  local -r alias_prefix="$command: aliased to "
+  local -r builtin_line="$command: shell built-in command"
+  local -r file_prefix='/'
 
-  local -r _function_start="$command () {"
-  local -r _function_end='}'
+  local -r function_start="$command () {"
+  local -r function_end='}'
 
-  local -r  _posix_exe_prefix='POSIX shell script text executable'
-  local -r _mach_o_bin_prefix='Mach-O universal binary'
-  local -r     _zsh_exe_infix='zsh script'
-  local -r   _bash_exe_prefix='Bourne-Again shell script'
-  local -r        _ascii_text='ASCII text'
+  local -r  posix_exe_prefix='POSIX shell script text executable'
+  local -r mach_o_bin_prefix='Mach-O universal binary'
+  local -r     zsh_exe_infix='zsh script'
+  local -r   bash_exe_prefix='Bourne-Again shell script'
+  local -r        ascii_text='ASCII text'
 
   # ————————————————————————————————————————————————————————————————— #
 
-  local -rA _types=(
+  local -rA types=(
         [alias]='1 0 alias'
       [builtin]='1 0 shell builtin'
      [function]='1 0 function'
@@ -73,7 +86,7 @@ wh() {
   )
 
   local -i 10 max_type_len=-1; local t
-  for t ("${(@k)_types}") if (( $#t > max_type_len )) max_type_len=${#t}
+  for t ("${(@k)types}") if (( $#t > max_type_len )) max_type_len=${#t}
 
   # ————————————————————————————————————————————————————————————————— #
 
@@ -97,7 +110,7 @@ wh() {
   if (( do_all )) dash_a='-a'
 
   which_output="$( which $dash_a -- "$command" )" || {
-    echo -E "$0: \`$command\` not found" >&2
+    echo -E "$error \`$command\` not found" >&2
     return 1
   }
 
@@ -110,58 +123,55 @@ wh() {
     # Function body & end
     if (( is_reading_func )) {
       # if we've reached the end, stop reading the function
-      if [[ "$line[1]" == "$_function_end" ]] is_reading_func=0
-
-      # make sure that escape chars aren't accidentally expanded
-      line="${line//\\/\\\\}"
+      if [[ "$line[1]" == "$function_end" ]] is_reading_func=0
 
       # put `then`/`do` on the same line as `if`/`for`/`while`
       # if [[ "$line" =~ '^\s*(then|do) *$' ]] {
-      if [[ "$line" == ' '#(then|do)' '# ]] {
+      if [[ "$line" == [[:space:]]#(then|do) ]] {
         all_definitions[-1]+="; ${(*)line/#[[:space:]]#}"
         continue
       }
 
-      all_definitions[-1]+=$'\n'"$line"
+      all_definitions[-1]+="$NL$line"
       continue
     }
 
     case "$line" {
 
       # function
-      ( "$_function_start" )
+      ( "$function_start" )
         is_reading_func=1
         def_type+=function
 
         func_path="$( wh::get_path "$command" )"
-        all_definitions+="$func_path${NL}function $line"
+        all_definitions+="${func_path:+$func_path$NL}function $line"
       ;;
 
       # alias
-      ( "$_alias_prefix"* )
+      ( "$alias_prefix"* )
         def_type+=alias
-        all_definitions+="${line/$_alias_prefix}"
+        all_definitions+="${line/$alias_prefix}"
       ;;
 
       # builtin
-      ( "$_builtin_line" )
+      ( "$builtin_line" )
         def_type+=builtin
         all_definitions+="$line"
       ;;
 
       # posix_exe, mach_o_bin, bash_exe, zsh_exe, general_exe, other_exe
-      ( "$_file_prefix"* )
+      ( "$file_prefix"* )
         all_definitions+="$line"
-        file_type="$( file -b "$line" )"
+        file_type="$( file --brief "$line" )"
 
         # only get the first line of the output
         case "${file_type/$NL*}" {
-         (  "$_posix_exe_prefix"*              ) def_type+=posix_exe   ;;
-         (  "$_mach_o_bin_prefix"*             ) def_type+=mach_o_bin  ;;
-         (  "$_bash_exe_prefix"*"$_ascii_text" ) def_type+=bash_exe    ;;
-         ( *"$_zsh_exe_infix"*"$_ascii_text"   ) def_type+=zsh_exe     ;;
-         (  "$_ascii_text"                     ) def_type+=general_exe ;;
-         ( *                                   ) def_type+=other_exe   ;;
+         (  "$posix_exe_prefix"*             ) def_type+=posix_exe   ;;
+         (  "$mach_o_bin_prefix"*            ) def_type+=mach_o_bin  ;;
+         (  "$bash_exe_prefix"*"$ascii_text" ) def_type+=bash_exe    ;;
+         ( *"$zsh_exe_infix"*"$ascii_text"   ) def_type+=zsh_exe     ;;
+         (  "$ascii_text"                    ) def_type+=general_exe ;;
+         ( *                                 ) def_type+=other_exe   ;;
         }
       ;;
 
@@ -174,8 +184,6 @@ wh() {
 
   # ——————————————————————————————————————————————————————————————————————— #
 
-  if (( draw_lines )) echo "$outside_line"
-
   local -ri 10 definition_count=${#all_definitions}
   local -ri 10    def_count_len=${#definition_count}
 
@@ -184,22 +192,26 @@ wh() {
   local type display_type body
   local -i 10 i run_func is_path
 
+  if (( definition_count == 1 )) do_lines=0
+
+  if (( do_lines )) echo "$outside_line"
+
   for i in {1.."$definition_count"}; {
     type="$def_type[i]"
     body="$all_definitions[i]"
 
-    if (( draw_lines && i != 1 )) echo "$inside_line"
+    if (( do_lines && i != 1 )) echo "$inside_line"
 
     # Print out the subtitle, which is:
     #  - the index, right-aligned,
     #    - so if there are >= 10 results, they the indices line up
     #  - a literal '.␣', to separate the index from the output type
-    if (( display_indices )) echo -n "${(l:$def_count_len:)i}. "
+    if (( do_indices )) echo -n "${(l:$def_count_len:)i}. "
 
     # I'm storing some extra data in the first few chars of the type names
-        run_func="${_types[$type][1]}"
-         is_path="${_types[$type][3]}"
-    display_type="${_types[$type][5,-1]}"
+        run_func="${types[$type][1]}"
+         is_path="${types[$type][3]}"
+    display_type="${types[$type][5,-1]}"
 
     if [[ "$type" != (function|alias|builtin) ]] || (( internal_mode )) {
       echo -E - "$display_type"
@@ -222,11 +234,12 @@ wh() {
     }
   }
 
-  if (( draw_lines )) echo "$outside_line"
+  if (( do_lines )) echo "$outside_line"
 
   return 0
 }
 
+# ——————————————————————————————————————————————————————————————————————————— #
 # ——————————————————————————————————————————————————————————————————————————— #
 
 wh::function() {
@@ -240,14 +253,6 @@ wh::function() {
 }
 
 wh::alias() {
-  local -r sq=\' dq=\"
-
-  local -r _equals_colour=$'\e[38;5;116m'  #94E2D5
-  local -r  _alias_colour=$'\e[38;5;183m'  #CBA6F7
-  local -r  _quote_colour=$'\e[38;5;114m'  #A6E3A1
-  local -r    _cmd_colour=$'\e[38;5;111m'  #89B4FA
-  local -r         _reset=$'\e[m'
-
   local           body="$1"
   local -r  visualiser="$2"
   local -r     command="$3"
@@ -257,17 +262,20 @@ wh::alias() {
   #  then show that command's `wh` entry instead
   # also check that we're not in internal mode, so we don't end up recursing
   setopt local_options rematch_pcre
+  local -a match mbegin mend
+  local    MATCH MBEGIN MEND
+
   local -i 2 in_secondary_mode=0
   if ! (( internal )) && [[ "$body" =~ '^ *(command +)?((\w|[-+.:])+) *$' ]] {
     local -r secondary_cmd="$match[2]"
-    local -r secondary_indent=$'\n\t\e[38;5;238m│\e[m '  #404454
+    local -r secondary_indent="$NL    $d_grey│$reset "
     in_secondary_mode=1
   }
 
   # —— Correctly Quote & Escape the Cmd Text ———————————————————————— #
 
   local quoted_body="${${(qqqq)body}[3,-2]}"
-  local -i 2 contains_{{s,d}q,sp}=0  # single quote, double quote, space
+  local -i 2 contains_{sq,dq,sp}=0  # single quote, double quote, space
 
   if (( ${#quoted_body//\\$sq} != $#quoted_body )) contains_sq=1
   if (( ${#quoted_body//$dq}   != $#quoted_body )) contains_dq=1
@@ -277,14 +285,18 @@ wh::alias() {
   if (( contains_sq && ! contains_dq )) {
     quoted_body="${quoted_body//\\$sq/$sq}"
     quote="$dq"
-  } elif ! (( contains_sq || contains_dq || contains_sp )) quote=
+
+  } elif ! (( contains_sq || contains_dq || contains_sp )) {
+    quote=
+
+  } # else quote=$sq
 
   # ————————————————————————————————————————————————————————————————— #
 
-  local -r alias_text="${_alias_colour}alias$_reset "
-  local -r   cmd_text="$_cmd_colour$command$_reset"
-  local -r     equals="$_equals_colour=$_reset"
-  local         quote="$_quote_colour$quote$_reset"
+  local -r alias_text="${pink}alias$reset "
+  local -r   cmd_text="$l_blue$command$reset"
+  local -r     equals="$turquoise=$reset"
+  local         quote="$green$quote$reset"
 
   # ————————————————————————————————————————————————————————————————— #
 
@@ -293,44 +305,37 @@ wh::alias() {
   local -r vis_command="${visualiser%% *}"
   local -ra vis_args=( "${(z)visualiser/$vis_command}" )
 
-  echo -nE - "$alias_text$cmd_text$equals$quote"
+  echo -nE "$alias_text$cmd_text$equals$quote"
 
   echo -nE "$quoted_body" \
     | command "$vis_command" "${(@)vis_args}" \
-    | sed "s/\\\'/$_cmd_colour\\\'$_quote_colour/g"
+    | sed "s/\\\'/$l_blue\\\'$green/g"
 
-  echo -nE "$quote"
+  echo -n "$quote"
 
   # ————————————————————————————————————————————————————————————————— #
 
-  if ! (( in_secondary_mode )) { echo; return 0; }
+  if ! (( in_secondary_mode )) echo && return
   body="${secondary_cmd:-$body}"
 
-  # the `//\\/\\\\` section is to make sure none of the
-  #  escape sequences (e.g. `\n`) get flattened
-  local -ra sub_cmd_lines=( "${(@f)"$( wh -x "$body" )"//\\/\\\\}" )
+  local -ra sub_cmd_lines=( "${(@f)$( wh -x -- "$body" )}" )
   local -ra sub_cmd_body=( "${(@)sub_cmd_lines[2,-1]}" )
-  local     sub_cmd_type="${sub_cmd_lines[1]}:"
+  local     sub_cmd_type="$sub_cmd_lines[1]:"
 
   local article='a'
-  if [[ "$sub_cmd_type[1]" =~ '[aeiou]' ]] article+='n'
+  if [[ "$sub_cmd_type" == [aeiou]* ]] article='an'
 
-  echo -nE ", where $_cmd_colour$body$_reset is $article $sub_cmd_type"
+  echo -nE ", where $l_blue$body$reset is $article $sub_cmd_type"
   echo -E "$secondary_indent${(pj:$secondary_indent:)sub_cmd_body}"
-
 }
 
 wh::builtin() {
-  local -r _cmd_colour=$'\e[38;5;111m'  #89B4FA
-  local -r _shl_colour=$'\e[38;5;131m'  #B84C50
-  local -r _reset=$'\e[m'
-
   # get the last part of the path of whichever shell they're using
   local -r shl_name="${${SHELL:-$BASH}/\/*\/}"
-  local -r cmd_name="${1/%:*}"
+  local -r cmd_name="${1%%:*}"
 
-  local -r shl_coloured="$_shl_colour$shl_name$_reset"
-  local -r cmd_coloured="$_cmd_colour$cmd_name$_reset"
+  local -r shl_coloured="$d_red$shl_name$reset"
+  local -r cmd_coloured="$l_blue$cmd_name$reset"
 
   echo -nE "$cmd_coloured is a $shl_coloured ("
   wh::echo_coloured_path "$SHELL"
@@ -338,62 +343,55 @@ wh::builtin() {
 }
 
 # ——————————————————————————————————————————————————————————————————————————— #
+# ——————————————————————————————————————————————————————————————————————————— #
 
 wh::echo_coloured_path() {
-  local -r   _leading_colour=$'\e[38;5;060m'    #353F5A
-  local -r      _body_colour=$'\e[38;5;068m'    #6D8EC5
-  local -r  _basename_colour=$'\e[38;5;131m'    #B84C50
-  local -r _underline_colour=$'\e[58;5;068;4m'  #628BCD
-
-  local -r _underline_off=$'\e[24m'
-  local -r _reset_all=$'\e[m'
-
   local -r path_="${1/$HOME/~}"
 
   local -r body="${${path_#*\/}%\/*}"
-  local -r leading="${path_%%\/*}"
+  local -r leading_segment="${path_%%\/*}"
   local -r basename="${path_##*\/}"
 
-  echo -nE "$_underline_colour"
-  echo -nE "$_leading_colour$leading/"
-  echo -nE "$_body_colour$body"
-  echo -nE "$_underline_off/$_underline_colour"
-  echo -nE "$_basename_colour$basename$_reset_all"
+  echo -nE "$blue_udln$d_blue$leading_segment/"
+  echo -nE "$m_blue$body"
+  echo -nE "$no_udln/$blue_udln"
+  echo -nE "$d_red$basename$reset"
 }
 
 wh::get_path() {
   local -r whence_out="$( whence -va "$1" )"
 
-  local abs_path="${${whence_out#*is a *function from }%$'\n'*}"
+  local abs_path="${${whence_out#*is a *function from }%$NL*}"
 
   local -i 2 do_rel_path=1
   local rel_path; rel_path="$(
-    grealpath --no-symlinks --relative-to=. "$abs_path" 2>/dev/null
-  )" || do_rel_path=0
+    grealpath --no-symlinks --relative-to=. -- "$abs_path"
+  )" 2>/dev/null || do_rel_path=0
 
   abs_path="${abs_path/#$HOME/~}"
 
-  echo -E "# $abs_path"
-
   if [[ "$rel_path" != '../'* ]] rel_path="./$rel_path"
 
-  if (( do_rel_path && $#rel_path <= $#abs_path )) \
+  echo -nE "${abs_path:+# $abs_path$NL}"
+
+  if (( do_rel_path && $#rel_path > 0 && $#rel_path <= $#abs_path )) \
     && [[ "$PWD" != "$HOME" && "$abs_path" != '~'* ]] {
     echo -E "# $rel_path"
   }
 }
 
 # ——————————————————————————————————————————————————————————————————————————— #
+# ——————————————————————————————————————————————————————————————————————————— #
 
 wh::__test__() {
   local -r col=$'\e[38;5;105m'
   local -r rst=$'\e[m'
 
-  local -ra _inputs_to_test=( config echo nv wh::__test__ )
+  local -ra _inputs_to_test=( pwda echo nv man - )  # spell:disable-line
 
   local input func title
   for input in "${(@)_inputs_to_test}"; {
-    title="${(r:$(( COLUMNS - $#input + 8 ))::─:)input/%/$col }"
+    title="${(r:$(( COLUMNS - $#input + 7 ))::─:)input/%/$col }"
     echo -E "$col───$rst $title $rst"
 
     # echo "${$( whence -va "$input" )/$HOME/~}"; echo
@@ -401,6 +399,7 @@ wh::__test__() {
   }
 }
 
+# ——————————————————————————————————————————————————————————————————————————— #
 # ——————————————————————————————————————————————————————————————————————————— #
 
 #  -v     Produce a more verbose report.
@@ -443,4 +442,4 @@ wh::__test__() {
 #         option.  This has the same effect as the -x option to the
 #         functions builtin.
 
-# spell:ignore grealpath
+# spell:ignore grealpath udln
